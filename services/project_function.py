0ee742bfbd933e2ea7ffc0c55db6faf419bc7ed7
JeromeCharbonneau <jerome.charbonneau@mail.utoronto.ca>
2026-05-31 20:22:22 -0400

    Update project_function.py with turnover penalty

--- services/project_function.py
+++ services/project_function.py
@@ -1,18 +1,40 @@
 from services.strategies import *
+import numpy as np
 
 
 def project_function(periodReturns, periodFactRet, x0=None):
     """
-    Please feel free to modify this function as desired
-    :param periodReturns:
-    :param periodFactRet:
-    :return: the allocation as a vector
+    Main project function.
+
+    This function is called by the main file.
+    It receives historical returns, factor returns, and previous/current portfolio weights.
     """
-    Strategy = RidgeLedoitWolf_MVO()
-    # Strategy = HistoricalMaxSharpe(
-    #     NumObs=36,
-    #     max_weight=0.25
-    # )
+
+    prev_weights = None
+
+    if x0 is not None:
+        x0_array = np.asarray(x0)
+
+        # If x0 is a matrix n x NoPeriods, find the last non-zero column
+        if x0_array.ndim == 2:
+            col_sums = np.sum(np.abs(x0_array), axis=0)
+            valid_cols = np.where(col_sums > 0)[0]
+
+            if len(valid_cols) > 0:
+                prev_weights = x0_array[:, valid_cols[-1]]
+
+        # If x0 is already a vector
+        elif x0_array.ndim == 1 and np.sum(np.abs(x0_array)) > 0:
+            prev_weights = x0_array
+
+    Strategy = RidgeLedoitWolf_MVO(
+        NumObs=48,
+        ridge_alpha=0.1,
+        lw_shrink_weight=0.7,
+        turnover_penalty=0.1,
+        prev_weights=prev_weights
+    )
 
     x = Strategy.execute_strategy(periodReturns, periodFactRet)
+
     return x
